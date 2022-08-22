42e59d0e6343a99653e4aa5241d53a311399e146
Jakub Bouček <pan@jakubboucek.cz>
2022-08-22 23:24:41 +0200

    Manualni autoload

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
+function autoload(string $className): void
+{
+    if (str_starts_with($className, 'App\\')) {
+        $file = str_replace(['App\\', '\\'], [__DIR__ . '/app/', '/'], $className) . '.php';
+        if(is_file($file)) {
+            require $file;
+        }
+    }
+}
+
+spl_autoload_register('autoload');
+
 \App\Trida::test();
 \App\Model\Driver::test();
