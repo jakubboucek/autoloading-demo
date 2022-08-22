61449cb780ea232412fe79bc239e24745c9ff14d
Jakub Bouček <pan@jakubboucek.cz>
2022-08-22 23:29:59 +0200

    Autoloading via Composer

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 
-function autoload(string $className): void
-{
-    if (str_starts_with($className, 'App\\')) {
-        $file = str_replace(['App\\', '\\'], [__DIR__ . '/app/', '/'], $className) . '.php';
-        if(is_file($file)) {
-            require $file;
-        }
-    }
-}
-
-spl_autoload_register('autoload');
+require __DIR__ . '/vendor/autoload.php';
 
 \App\Trida::test();
 \App\Model\Driver::test();
